7b60649662767a4f0b89d4a32922a18dd915d179
@Marcelinha <131552194+Marcela1204@users.noreply.github.com>
2026-03-12 05:51:21 +0000

    commit

---
+++ SpaceShip/SpaceShip/Assets/Scripts/Bullet.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 10f;
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        Destroy(gameObject, 5f);
+    }
+}
